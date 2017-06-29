82459a8156dd729a09ae0f993ff5a1869be593a9
Victor Leach <victor96@mac.com>
2017-06-29 12:53:42 +0100

    Updated error codes.

--- src/BUError.ts
+++ src/BUError.ts
@@ -1,18 +1,18 @@
 export enum BUError {
-
-	//! Core,
+	
+	//! Core
 	Unknown = 100,
 	Server = 200,
 	NotFound = 300,
 	Connection = 400,
 	Json = 500,
-	
-	//! Users,
+
+	//! Users
 	USR_Invalid = 2000,
 	USR_NotFound = 2001,
 	USR_Disabled = 2002,
 	USR_Access = 2003,
-	
+
 	USR_InvalidLogin = 2100,
 	USR_InvalidUsername = 2101,
 	USR_InvalidEmail = 2102,
@@ -21,23 +21,23 @@ export enum BUError {
 	USR_InvalidStatus = 2105,
 	USR_InvalidProjects = 2106,
 	USR_InvalidProject = 2107,
-	
+
 	USR_IncorrectLogin = 2200,
-	
+
 	USR_ExistingUsername = 2300,
 	USR_ExistingEmail = 2301,
-	
+
 	//! Sessions
 	SES_Invalid = 3000,
 	SES_Incorrect = 3001,
 	SES_Disabled = 3002,
-	
+
 	//! Projects
 	PRJ_Invalid = 41000,
 	PRJ_NotFound = 41001,
 	PRJ_Incorrect = 41002,
 	PRJ_Access = 41003,
-	
+
 	PRJ_InvalidName = 41100,
 	PRJ_InvalidVisible = 41101,
 	PRJ_InvalidSubtitle = 41102,
@@ -45,20 +45,47 @@ export enum BUError {
 	PRJ_InvalidBody = 41104,
 	PRJ_InvalidIcon = 41105,
 	PRJ_InvalidImage = 41106,
-	
-	//! Access Keys,
-	PRJ_ACK_Invalid = 42000,
-	PRJ_ACK_NotFound = 42001,
-	PRJ_ACK_Incorrect = 42002,
-	
-	PRJ_ACK_InvalidName = 42100,
-	PRJ_ACK_InvalidStatus = 42101,
-	
-	//! Collections,
-	PRJ_COL_Invalid = 43000,
-	PRJ_COL_NotFound = 43001,
-	PRJ_COL_Access = 43002,
-	
-	PRJ_COL_InvalidName = 43100,
-	PRJ_COL_InvalidBody = 43101
+
+	//! Access Keys
+	ACK_Invalid = 42000,
+	ACK_NotFound = 42001,
+	ACK_Incorrect = 42002,
+
+	ACK_InvalidName = 42100,
+	ACK_InvalidStatus = 42101,
+
+	//! Collections
+	COL_Invalid = 43000,
+	COL_NotFound = 43001,
+	COL_Access = 43002,
+
+	COL_InvalidSize = 43100,
+	COL_InvalidPage = 43101,
+	COL_InvalidSort = 43102,
+	COL_InvalidDir = 43103,
+	COL_InvalidName = 43104,
+	COL_InvalidBody = 43105,
+
+	//! Visuals
+	VIS_Invalid = 44000,
+	VIS_NotFound = 44001,
+	VIS_Access = 44002,
+
+	VIS_InvalidCollection = 44100,
+	VIS_InvalidName = 44101,
+	VIS_InvalidType = 44102,
+	VIS_InvalidOptions = 44103,
+
+	VIS_IncorrectCollection = 44200,
+	VIS_IncorrectType = 44201,
+
+	//! Applications
+	APP_MissingContName = 51000,
+	APP_MissingContEmail = 51001,
+	APP_MissingContPhone = 51002,
+	APP_MissingOrgName = 51003,
+	APP_MissingOrgAddress = 51004,
+	APP_MissingProjDuration = 51005,
+	APP_MissingProjPurpose = 51006,
+	APP_MissingProjDescription = 51007
 }
